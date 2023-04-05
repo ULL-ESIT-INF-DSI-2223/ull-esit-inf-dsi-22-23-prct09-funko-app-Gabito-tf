58b4a3bca935e1ea657108f5c2713571f4763836
Gabi <Alu0101098340@ull.edu.es>
2023-04-05 20:52:19 +0000

    Codigo finlizado

---
+++ src/funko/yargs.ts
@@ -0,0 +1,326 @@
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+import { Funko } from './funko';
+
+import * as fs from 'fs';
+import * as path from 'path'
+import chalk = require('chalk');
+
+yargs(hideBin(process.argv))
+
+  .command('add', 'Adds a funko', {
+    id: {
+      description: 'Funko ID',
+      type: 'number',
+      demandOption: true
+    },
+    usuario: {
+      description: 'Usuario',
+      type: 'string',
+      demandOption: true
+    },
+    nombre: {
+      description: 'Nombre del funko',
+      type: 'string',
+      demandOption: true
+    },
+    desc: {
+      description: 'Descripcion del funko',
+      type: 'string',
+      demandOption: true
+    },
+    tipo: {
+      description: 'Tipo de funko',
+      type: 'string',
+      demandOption: true
+    },
+    genero: {
+      description: 'Genero del funko',
+      type: 'string',
+      demandOption: true
+    },
+    franquicia: {
+      description: 'Franquicia del funko',
+      type: 'string',
+      demandOption: true
+    },
+    numero: {
+      description: 'Numero de la franquicia del funko',
+      type: 'number',
+      demandOption: true
+    },
+    exclusivo: {
+      description: '¿Es un funko exclusivo?',
+      type: 'boolean',
+      demandOption: true
+    },
+    especial: {
+      description: 'Caracteristicas especiales del funko',
+      type: 'string',
+      demandOption: true
+    },
+    valor: {
+      description: 'valor del funko',
+      type: 'number',
+      demandOption: true
+    }
+
+  }, (argv) => {
+    const dirPath = `users/${argv.usuario}`;
+    let existe = false;
+    if (fs.existsSync(dirPath)) {
+      const files = fs.readdirSync(dirPath);
+      files.forEach(file => {
+        const filePath = `${dirPath}/${file}`;
+        const data = fs.readFileSync(filePath, 'utf-8');
+        const funkoJSON = JSON.parse(data); 
+        if (funkoJSON.id === argv.id) {
+          console.log(chalk.red.bold(`Ya existe un Funko con el ID = ${argv.id} en la colección de ${argv.usuario}`));
+          existe = true;
+        }
+      });  
+          
+      if (!existe) {
+        const funkosJSON = {
+          id: argv.id,
+          nombre: argv.nombre,
+          descripcion: argv.desc,
+          tipo: argv.tipo,
+          genero: argv.genero,
+          franquicia: argv.franquicia,
+          numero: argv.numero,
+          exclusivo: argv.exclusivo,
+          caracteristicasEspeciales: argv.especial,
+          valor: argv.valor        
+        }
+        fs.writeFileSync(`${dirPath}/${argv.id}.json`, JSON.stringify(funkosJSON, null, 2));
+        console.log(chalk.green.bold(`Nuevo Funko con el ID = ${argv.id} se ha añadido a la colección de ${argv.usuario}`));
+      }
+    } else {
+      console.log(chalk.red.bold(`El usuario ${argv.usuario} no tiene una colección`));
+      return;
+    }
+  })
+  
+  /**
+   * Command update, to update a Funko from a usuario list.
+   */
+  .command('update', 'update a funko', {
+    id: {
+      description: 'Funko ID',
+      type: 'number',
+      demandOption: true
+    },
+    usuario: {
+      description: 'Usuario',
+      type: 'string',
+      demandOption: true
+    },
+    nombre: {
+      description: 'Nombre del funko',
+      type: 'string',
+      demandOption: true
+    },
+    desc: {
+      description: 'Descripcion del funko',
+      type: 'string',
+      demandOption: true
+    },
+    tipo: {
+      description: 'Tipo de funko',
+      type: 'string',
+      demandOption: true
+    },
+    genero: {
+      description: 'Genero del funko',
+      type: 'string',
+      demandOption: true
+    },
+    franquicia: {
+      description: 'Franquicia del funko',
+      type: 'string',
+      demandOption: true
+    },
+    numero: {
+      description: 'Numero de la franquicia del funko',
+      type: 'number',
+      demandOption: true
+    },
+    exclusivo: {
+      description: '¿Es un funko exclusivo?',
+      type: 'boolean',
+      demandOption: true
+    },
+    especial: {
+      description: 'Caracteristicas especiales del funko',
+      type: 'string',
+      demandOption: true
+    },
+    valor: {
+      description: 'valor del funko',
+      type: 'number',
+      demandOption: true
+    }
+
+  }, (argv) => {
+    const dirPath = `users/${argv.usuario}`;
+    let existe = false;
+    if (fs.existsSync(dirPath)) {
+      const files = fs.readdirSync(dirPath);
+      files.forEach(file => {
+        const filePath = `${dirPath}/${file}`;
+        const data = fs.readFileSync(filePath, 'utf-8');
+        const funkoJSON = JSON.parse(data); 
+        if (funkoJSON.id === argv.id) {          
+          existe = true;
+          if (argv.name != undefined) {   
+            fs.renameSync(filePath, `${dirPath}/${argv.name}.json`)         
+            funkoJSON.name = argv.name;
+          } else {
+            const fileName = path.parse(filePath).name;
+            funkoJSON.name = fileName;
+          }
+          if (argv.desc != undefined) {
+            funkoJSON.desc = argv.desc;
+          }
+          if (argv.type != undefined) {
+            funkoJSON.type = argv.type;
+          }
+          if (argv.genre != undefined) {
+            funkoJSON.genre = argv.genre;
+          }
+          if (argv.franch != undefined) {
+            funkoJSON.franch = argv.franch;
+          }
+          if (argv.franchnum != undefined) {
+            funkoJSON.franchnum = argv.franchnum;
+          }
+          if (argv.excl != undefined) {
+            funkoJSON.excl = argv.excl;
+          }
+          if (argv.spcfeat != undefined) {
+            funkoJSON.spcfeat = argv.spcfeat;
+          }
+          if (argv.market != undefined) {
+            funkoJSON.marketValue = argv.market;
+          }
+          fs.writeFileSync(`${dirPath}/${funkoJSON.name}.json`, JSON.stringify(funkoJSON, null, 2));
+          console.log(chalk.green.bold(`El Funko con el ID = ${argv.id} ha sido actualizado en la colección de ${argv.usuario}`));
+          existe = true;
+        }        
+      });
+      if (!existe) {
+        console.log(chalk.red.bold(`No existe ningún funko con el ID = ${argv.id} en la colección de ${argv.usuario}`));
+      }  
+    } else {
+      console.log(chalk.red.bold(`El usuario ${argv.usuario} no tiene una colección`));
+      return;
+    }
+  })
+
+
+    .command('remove', 'Remove a funko from the collection', {
+      id: {
+        description: 'Funko ID',
+        type: 'number',
+        demandOption: true
+      },
+      usuario: {
+        description: 'Usuario',
+        type: 'string',
+        demandOption: true
+      }
+  }, (argv) => {
+    const dirPath = `users/${argv.usuario}`;
+      if (fs.existsSync(dirPath)) {
+        let found = false;
+        const files = fs.readdirSync(dirPath);
+        files.forEach(file => {
+          const filePath = `${dirPath}/${file}`;
+          const data = fs.readFileSync(filePath, 'utf-8');
+          const funkoJSON = JSON.parse(data);
+          if (funkoJSON.id === argv.id) {
+            fs.unlinkSync(filePath);
+            console.log(chalk.green.bold(`Se ha eliminado correctamente el Funko con el ID = ${argv.id} en la colección de ${argv.usuario}`))
+            found = true;       
+          }    
+        });
+        if (!found) {
+          console.log(chalk.red.bold(`No existe ningún Funko con el ID = ${argv.id} en la colección de ${argv.usuario}`));
+        }
+      } else {
+        console.log(chalk.red.bold(`El usuario ${argv.usuario} no tiene una colección`));
+        return;
+      }
+  })
+
+
+  .command('list', 'List a funko collection', {
+    usuario: {
+      description: 'Usuario',
+      type: 'string',
+      demandOption: true
+    }
+  }, (argv) => {
+    const dirPath = `users/${argv.usuario}`;
+    if (fs.existsSync(dirPath)) {
+      const files = fs.readdirSync(dirPath);
+      console.log(chalk.white.bold(`${argv.usuario} Funko Pop collection`));
+      console.log("----------------------------");
+      files.forEach(file => {
+        const filePath = `${dirPath}/${file}`;
+        const data = fs.readFileSync(filePath, 'utf-8');
+        const funkoJSON = JSON.parse(data);        
+        const funko = new Funko(funkoJSON.id, funkoJSON.nombre, funkoJSON.descripcion, funkoJSON.tipo, funkoJSON.genero, funkoJSON.franquicia, funkoJSON.numero, funkoJSON.exclusivo, funkoJSON.caracteristicasEspeciales, funkoJSON.valor);
+        console.log(funko.print());    
+        console.log("----------------------------");  
+      });
+    } else {
+      console.log(chalk.red.bold(`El usuario ${argv.usuario} no tiene una colección`));
+      return;
+    }
+  })
+
+  .command('read', 'Show a concrete funko from the collection', {
+    id: {
+      description: 'Funko ID',
+      type: 'number',
+      demandOption: true
+    },
+    usuario: {
+      description: 'Usuario',
+      type: 'string',
+      demandOption: true
+    }
+  }, (argv) => {
+    const dirPath = `users/${argv.usuario}`;
+    if (fs.existsSync(dirPath)) {
+      let found = false;
+      const files = fs.readdirSync(dirPath);
+      files.forEach(file => {
+        const filePath = `${dirPath}/${file}`;
+        const data = fs.readFileSync(filePath, 'utf-8');
+        const funkoJSON = JSON.parse(data);
+        if (funkoJSON.id === argv.id) {
+          const funko = new Funko(funkoJSON.id, funkoJSON.nombre, funkoJSON.descripcion, funkoJSON.tipo, funkoJSON.genero, funkoJSON.franquicia, funkoJSON.numero, funkoJSON.exclusivo, funkoJSON.caracteristicasEspeciales, funkoJSON.valor);      
+          console.log(funko.print());   
+          found = true;       
+        }    
+      });
+      if (found == false) {
+        console.log(chalk.red.bold(`No existe ningún funko con el ID = ${argv.id} en la colección de ${argv.usuario}`));
+      }
+    } else {
+      console.log(chalk.red.bold(`El usuario ${argv.usuario} no tiene una colección`));
+      return;
+    }
+  })
+  .help()
+  .argv;
+
+
+
+
+
+
---
+++ tests/funko.spec.ts
@@ -0,0 +1,94 @@
+import "mocha";
+import { expect } from "chai";
+import { Funko } from "../src/funko/funko"
+import { Tipo, Genero} from "../src/funko/funko";
+
+const funkoTests = new Funko(999, "Test Funko", "Test Funko", Tipo.POP, Genero.ANIME, "Pokémon", 999, true, "Test Funko", 999);
+
+describe('Tests clase Funk', () => {
+  it ('Setear ID de funko', () => {
+    funkoTests.setId(999);
+    expect(funkoTests.getId()).to.equal(999);
+  });
+  it ('Ver id de funko', () => {
+    expect(funkoTests.getId()).to.equal(999);
+  });
+  it ('Setear nombre de funko', () => {
+    funkoTests.setNombre("Test Funko");
+    expect(funkoTests.getNombre()).to.equal("Test Funko");
+  });
+  it ('Ver nombre de funko', () => {
+    expect(funkoTests.getNombre()).to.equal("Test Funko");
+  });
+  it ('Setear descripción de funko', () => {
+    funkoTests.setDescripcion("Test Funko");
+    expect(funkoTests.getDescripcion()).to.equal("Test Funko");
+  });
+  it ('Ver descripción de funko', () => {
+    expect(funkoTests.getDescripcion()).to.equal("Test Funko");
+  });
+  it ('Setear tipo de funko', () => {
+    funkoTests.setTipo(Tipo.POP);
+    expect(funkoTests.getTipo()).to.equal(Tipo.POP);
+  });
+  it ('Ver tipo de funko', () => {
+    expect(funkoTests.getTipo()).to.equal(Tipo.POP);
+  });
+  it ('Setear género de funko', () => {
+    funkoTests.setGenero(Genero.ANIME);
+    expect(funkoTests.getGenero()).to.equal(Genero.ANIME);
+  });
+  it ('Ver género de funko', () => {
+    expect(funkoTests.getGenero()).to.equal(Genero.ANIME);
+  });
+  it ('Setear franquicia de funko', () => {
+    funkoTests.setFranquicia("Pokémon");
+    expect(funkoTests.getFranquicia()).to.equal("Pokémon");
+  });
+  it ('Ver franquicia de funko', () => {
+    expect(funkoTests.getFranquicia()).to.equal("Pokémon");
+  });
+  it ('Setear número de funko', () => {
+    funkoTests.setNumero(999);
+    expect(funkoTests.getNumero()).to.equal(999);
+  });
+  it ('Ver número de funko', () => {
+    expect(funkoTests.getNumero()).to.equal(999);
+  });
+  it ('Setear exclusivo de funko', () => {
+    funkoTests.setExclusivo(true);
+    expect(funkoTests.getExclusivo()).to.equal(true);
+  });
+  it ('Ver exclusivo de funko', () => {
+    expect(funkoTests.getExclusivo()).to.equal(true);
+  });
+  it ('Setear características especiales de funko', () => {
+    funkoTests.setCaracteristicasEspeciales("Test Funko");
+    expect(funkoTests.getCaracteristicasEspeciales()).to.equal("Test Funko");
+  });
+  it ('Ver características especiales de funko', () => {
+    expect(funkoTests.getCaracteristicasEspeciales()).to.equal("Test Funko");
+  });
+  it ('Setear valor de funko', () => {
+    funkoTests.setValor(10);
+    expect(funkoTests.getValor()).to.equal(10);
+  });
+  it ('Ver valor de funko', () => {
+    expect(funkoTests.getValor()).to.equal(10);
+  });
+  it('Imprimir funko con valor muy bajo', () => {
+    expect(funkoTests.print()).to.not.be.null;
+  });
+  it('Imprimir funko con valor bajo', () => {
+    funkoTests.setValor(40);
+    expect(funkoTests.print()).to.not.be.null;
+  });
+  it('Imprimir funko con valor medio', () => {
+    funkoTests.setValor(60);
+    expect(funkoTests.print()).to.not.be.null;
+  });
+  it('Imprimir funko con valor alto', () => {
+    funkoTests.setValor(90);
+    expect(funkoTests.print()).to.not.be.null;
+  });
+})
